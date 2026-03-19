d06b9e23b8bb4d62e342716b6a0fa45ac472f027
Shivesh Singh Rao <1ds20is100@dsce.edu.in>
2026-03-19 23:17:50 +0530

    first poly comment

---
+++ polymorphism/1_test.py
@@ -0,0 +1,35 @@
+def check_2(num: int , str: str):
+    print(f"hello bunny: {num} and str is {str}")
+
+def check_2(num:int):
+    print(f"hello bunny with only num:{num}")
+
+class A:
+    def check(num: int , str: str):
+        print(f"hello num: {num} and str is {str}")
+    
+    def check(num:int):
+        print(f"hello num:{num}")
+
+
+
+# obj = A()
+# obj.check(1)
+check_2(1)
+check_2(1,"bob")
+
+
+"""
+
+In Python, function overloading does NOT exist (at least not like Java/C++).
+
+So what happens?
+
+The second check_2 function overrides the first one
+
+Python only keeps the latest definition
+
+So effectively, Python sees only this:
+def check_2(num:int):
+    print(f"hello bunny with only num:{num}")
+"""
