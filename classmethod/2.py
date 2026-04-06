470c1b678181282d56913a7f327e3d426ae382a7
Shivesh Singh Rao <1ds20is100@dsce.edu.in>
2026-04-07 00:00:04 +0530

    somthign

---
+++ classmethod/2.py
@@ -0,0 +1,9 @@
+# when to use this 
+# when logic is related to class not a specific object
+# mostly by factory pattern
+
+class User:
+    def __init__(self, name, age):
+        self.name= name
+        self.age= age
+        
