828430dd53750c9a0e74933dc77efed915eb330b
Shivesh Singh Rao <1ds20is100@dsce.edu.in>
2026-04-12 23:52:00 +0530

    json

---
+++ elements/4.py
@@ -0,0 +1,18 @@
+import json
+
+# JSON string
+data = '{"name": "Rao", "age": 25, "skills": ["Python", "JS"]}'
+
+# Convert JSON -> Python dict
+parsed = json.loads(data)
+
+print(parsed["name"])
+print(parsed["skills"][0])
+
+# Modify
+parsed["age"] = 26
+
+# Convert back to JSON
+json_data = json.dumps(parsed, indent=2)
+
+print(json_data)
